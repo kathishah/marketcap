16ac9cd44febec98998fd58f3e557d1922cda648
kathi.shah@gmail.com <kathi.shah@gmail.com>
2012-04-13 23:21:16 -0700

    another commit?

--- web.rb
+++ web.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'json'
+
+@@lookup = JSON.parse(IO.read("lookup.json"))
 
 get '/' do
-	"Hello Taral"
+	"Hello dash"
 end
+
+get '/lookup/:key' do
+  if @@lookup.has_key?params[:key]
+    if params[:sub_key]
+      @@lookup[params[:key]][params[:sub_key]].to_json
+    else
+      @@lookup[params[:key]].to_json
+    end
+  else
+    "Not found"
+  end
+end
+
+get '/refresh/lookup' do
+  @@lookup = JSON.parse(IO.read("lookup.json"))
+end
+
