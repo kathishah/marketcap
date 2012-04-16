f4582b4a055887a2ac87a7e0511ec19fcaa8592f
kathi.shah@gmail.com <kathi.shah@gmail.com>
2012-04-16 01:54:37 -0700

    Added symbols per industry; implemented first query - large,growing,cheap

--- web.rb
+++ web.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+require 'cgi'
 require 'json'
+require 'net/http'
+require 'uri'
 
 @@lookup = JSON.parse(IO.read("lookup.json"))
+QUERY_URL = "http://query.yahooapis.com/v1/public/yql?q=sql%0A%09%09&format=json&env=" + CGI.escape("http://datatables.org/alltables.env")
 
 get '/' do
   usage =	"Usage:<br/>" + "<table><tr><td>/lookup/</td></tr>" +
@@ -13,6 +17,53 @@ get '/' do
   "<tr><td>&nbsp;</td><td>yahoo.finance.industry_by_sector?sub_key=sector_name</td></tr>" +
   "</table>"
   usage
+end
+
+get '/large_growing_cheap/:sector' do
+  industries = @@lookup['yahoo.finance.industry_by_sector'][params[:sector]].keys.sort
+  # load symbols
+  symbols = []
+  @@lookup['symbol_by_industry'].each { |ind|
+    next unless industries.sort.index(ind["id"])
+    ind['company'].each { |c| symbols << c['symbol'] }
+  }
+  logger.info("Found #{symbols.length} symbols")
+  query = "select symbol from yahoo.finance.quotes where " +
+    "MarketCapitalization > 5 and " + #5B
+    "PERatio <= 20 and " +
+    "PriceSales <= 1.3 and " #P/S
+    #TODO Earnings growth estimate
+  results = []
+  if symbols.size > 100
+    batches = symbols.size / 100 + 1
+    (1..batches).each { |batch_id|
+      index_start = (batch_id - 1) * 100
+      index_end   = (batch_id * 100) - 1
+      index_end   = symbols.size - 1 if index_end > (symbols.size - 1)
+      q = query + 'symbol in ("' + symbols[index_start..index_end].join('","') + '")'
+      logger.info(q)
+      url = QUERY_URL.gsub("sql", CGI.escape(q))
+      logger.info(url)
+      response = Net::HTTP.get_response(URI.parse(url))
+      logger.info(response)
+      if response.is_a?Net::HTTPOK
+        j_response = JSON.parse(response.body)
+        results << j_response['query']['results']['quote']
+      end
+    }
+  else
+    query = query + 'symbol in ("' + symbols.join('","') + '")'
+    logger.info(query)
+    url = QUERY_URL.gsub("sql", CGI.escape(query))
+    logger.info(url)
+    response = Net::HTTP.get_response(URI.parse(url))
+    logger.info(response.inspect)
+    if response.is_a?Net::HTTPOK
+      j_response = JSON.parse(response.body)
+      results << j_response['query']['results']['quote']
+    end
+  end
+  JSON.pretty_generate(results.flatten)
 end
 
 get '/lookup/:key' do
