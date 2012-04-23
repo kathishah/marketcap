49f2610d8e52244763648ba860eff6ac297053d9
kathi.shah@gmail.com <kathi.shah@gmail.com>
2012-04-23 00:13:42 -0700

    Added preset/largest_market_cap and present/highest_volume. Also refactored code

--- web.rb
+++ web.rb
@@ -29,7 +29,7 @@ helpers do
   # Constructs query using the symbols. Uses filter_proc to filter the results on the way out.
   # Especially useful for filtering based on market cap because the data may look like 7.034B or
   # 11.213M
-  def run_query(symbols, query, filter_proc=nil)
+  def run_query(symbols, query, filter_proc=nil, sort_proc=nil)
     results = []
     if symbols.size > 100
       # create batches so that the GET has less than 4096 chars
@@ -38,15 +38,16 @@ helpers do
         index_start = (batch_id - 1) * 100
         index_end   = (batch_id * 100) - 1
         index_end   = symbols.size - 1 if index_end > (symbols.size - 1)
+        logger.info("Running batch: #{index_start} - #{index_end} of #{symbols.size}")
         q = query + 'symbol in ("' + symbols[index_start..index_end].join('","') + '")'
-        logger.info(q)
+        logger.debug(q)
         url = QUERY_URL.gsub("sql", CGI.escape(q))
-        logger.info(url)
+        logger.debug(url)
         response = Net::HTTP.get_response(URI.parse(url))
-        logger.info(response)
+        logger.debug(response)
         if response.is_a?Net::HTTPOK
           j_response = JSON.parse(response.body)
-          logger.info(j_response)
+          logger.debug(j_response)
           if filter_proc
             results << j_response['query']['results']['quote'].select { |x| filter_proc.call(x) }
           else
@@ -58,12 +59,12 @@ helpers do
       query = query + 'symbol in ("' + symbols.join('","') + '")'
       logger.info(query)
       url = QUERY_URL.gsub("sql", CGI.escape(query))
-      logger.info(url)
+      logger.debug(url)
       response = Net::HTTP.get_response(URI.parse(url))
       logger.info(response.inspect)
       if response.is_a?Net::HTTPOK
         j_response = JSON.parse(response.body)
-        logger.info(j_response)
+        logger.debug(j_response)
         if j_response and j_response.has_key?'query' and j_response['query'].has_key?'results' and j_response['query']['results']
           if filter_proc
             results << j_response['query']['results']['quote'].select { |x| filter_proc.call(x) }
@@ -73,7 +74,20 @@ helpers do
         end
       end
     end
+    results.flatten!
+    logger.debug("Final results before sort: #{results}")
+    results.sort!{ |a,b| sort_proc.call(a,b) } if sort_proc
+    logger.debug("Final results after sort: #{results}")
     results
+  end
+
+  def get_market_cap(str)
+    -1 unless str
+     if str.end_with?'B' 
+       str.gsub('B', '').to_i*(10**9)
+     else 
+       str.gsub('M','').to_i*(10**6) 
+     end
   end
 end
 
@@ -96,26 +110,60 @@ get '/preset/:preset_name/:sector' do
   end
   logger.info("Found #{symbols.length} symbols")
   # create query
-  query, special_condition = case
+  query, special_condition, sort_by = case
           when params[:preset_name] == "large_growing_cheap"
-            ["select symbol,LastTradePriceOnly,Volume,MarketCapitalization from yahoo.finance.quotes where " +
-            "PERatio <= 20 and " +
-            "PriceSales <= 1.3 and ", #P/S
-            Proc.new { |x|
+            ["select symbol,Name,LastTradePriceOnly,Volume,MarketCapitalization from yahoo.finance.quotes where " +
+              "PERatio <= 20 and " +
+              "PriceSales <= 1.3 and ", 
+            Proc.new { |x| #filter
               if x["MarketCapitalization"]
-                mc = if x["MarketCapitalization"].end_with?'B'; x["MarketCapitalization"].gsub('B', '').to_i*(10**9); else; x["MarketCapitalization"].gsub('M','').to_i*(10**6); end
+                mc = get_market_cap(x["MarketCapitalization"])
               else
                 mc = 0
               end
               mc >= 5*(10**9) #5B
-            }]
+            },
+            nil #sort
+            ]
             #TODO Earnings growth estimate
-#          when params[:preset_name] == "strong_forecasted_growth"
-#            "select symbol,LastTradePriceOnly,Volume from yahoo.finance.quotes where " +
-#            "MarketCapitalization >= 5 and " + #5B
-#
+          when params[:preset_name] == "largest_market_cap"
+            ["select symbol,Name,MarketCapitalization,LastTradePriceOnly,Volume from yahoo.finance.quotes where ", #query
+              Proc.new { |x| #filter
+                if x["MarketCapitalization"]
+                  mc = get_market_cap(x["MarketCapitalization"])
+                else
+                  mc = 0
+                end
+                mc >= 50*(10**9) #50B
+              },
+              Proc.new { |x,y| #sort
+                begin 
+                  get_market_cap(y["MarketCapitalization"]) <=> get_market_cap(x["MarketCapitalization"])
+                rescue
+                  0
+                end 
+              } 
+            ]
+          when params[:preset_name] == "highest_volume"
+            ["select symbol,Name,MarketCapitalization,LastTradePriceOnly,Volume from yahoo.finance.quotes where ", #query
+              Proc.new { |x| #filter
+                vol = 0
+                vol = x["Volume"].to_i if x["Volume"]
+                vol >= 10*(10**6) #10M
+              },
+              Proc.new { |x,y| #sort
+                begin 
+                  y["Volume"] <=> x["Volume"]
+                rescue
+                  0
+                end 
+              } 
+            ]
+          #TODO: get sales ttm data for largest_sales_revenue  
+          else 
+            halt 404, "possible presets: large_growing_cheap; largest_market_cap;"
           end
-  results = run_query(symbols, query, special_condition)
+  results = run_query(symbols, query, special_condition, sort_by)
   JSON.pretty_generate(results.flatten)
 end
 
